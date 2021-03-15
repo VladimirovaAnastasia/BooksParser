7cddc365bbe85b1d598cf768843a9ce08df26cf3
KRASAVA <vladimirova.a-a@yandex.ru>
2021-03-15 20:09:05 +0400

    Resolve mentor's comments

--- script.py
+++ script.py
@@ -6,14 +6,11 @@ from pathlib import Path
 import logging
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlsplit, unquote
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 logger = logging.getLogger()
-
-Path("books").mkdir(parents=True, exist_ok=True)
-Path("images").mkdir(parents=True, exist_ok=True)
 
 
 def create_parser():
@@ -30,14 +27,15 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def download_txt(url, filename, folder='books/'):
-    response = requests.get(url, verify=False, allow_redirects=False)
+def download_txt(url, filename, book_id, folder='books/'):
+    payload = {"id": f"{book_id}"}
+    response = requests.get(url, verify=False, allow_redirects=False, params=payload)
     check_for_redirect(response)
 
     valid_filename = sanitize_filename(filename)
     filename = os.path.join(folder, valid_filename)
-    with open(filename, 'wb') as file:
-        file.write(response.content)
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(response.text)
 
 
 def download_image(url, filename, folder='images/'):
@@ -49,18 +47,24 @@ def download_image(url, filename, folder='images/'):
     with open(filename, 'wb') as file:
         file.write(response.content)
 
+def parse_img_name(file_url):
+    path = urlsplit(file_url).path
+    encoding_path = unquote(path)
+    filename = os.path.split(encoding_path)[-1]
+    return filename
+
 
 def parse_book_page(soup):
     title_tag = soup.find('h1')
     title_text = title_tag.text.split('::')
-    [book_title, book_author] = title_text
+    book_title, book_author = title_text
 
     book_genres = soup.find('span', class_='d_book').find_all('a')
     book_comments = soup.find_all('div', class_='texts')
 
     book_img = soup.find('div', class_='bookimage').find('img')['src']
 
-    return [book_title, book_author, book_genres, book_comments, book_img]
+    return book_title, book_author, book_genres, book_comments, book_img
 
 
 def get_book(book_id):
@@ -69,7 +73,7 @@ def get_book(book_id):
     check_for_redirect(response)
 
     soup = BeautifulSoup(response.text, 'lxml')
-    [book_title, book_author, book_genres, book_comments, book_img] = parse_book_page(soup)
+    book_title, book_author, book_genres, book_comments, book_img = parse_book_page(soup)
 
     logger.info(f'Заголовок: {book_title.strip()}')
     logger.info(f'Автор: {book_author.strip()}')
@@ -82,16 +86,19 @@ def get_book(book_id):
         comment_text = comment.find('span').text
         logger.info(f'{comment_text}')
 
-    book_text_link = f'https://tululu.org/txt.php?id={book_id}'
+    book_text_link = 'https://tululu.org/txt.php'
     book_filename = f'{book_id}. {book_title}'
-    download_txt(book_text_link, book_filename)
+    download_txt(book_text_link, book_filename, book_id)
 
-    img_filename = book_img.split('/')[-1]
+    img_filename = parse_img_name(book_img)
     book_img_link = urljoin('https://tululu.org/', book_img)
     download_image(book_img_link, img_filename)
 
 
 def main():
+    Path("books").mkdir(parents=True, exist_ok=True)
+    Path("images").mkdir(parents=True, exist_ok=True)
+
     logging.basicConfig(
         level=logging.INFO,
         datefmt='%Y-%m-%d %H:%M:%S',
